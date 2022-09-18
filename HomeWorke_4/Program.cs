19460b0487e829a0ced39839216d3726e5005290
Olga <olga_hom@mail.ru>
2022-09-18 12:12:28 +0300

    Задача4

---
+++ HomeWorke_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//  5 -> 2, 4
+//  8 -> 2, 4, 6, 8
+Console.Clear();
+Console.WriteLine("Введите число");
+int namber = int.Parse(Console.ReadLine());
+for (int i = 1; i <= namber; i++)
+{
+    if (i %2 == 0)
+       Console.Write( i + " ");
+     } 
+ Console.WriteLine(" Это четные числа в промежутке от 1 до " + namber + ".");
